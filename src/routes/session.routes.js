f8dc39d2b06d7f337f68021b20c342e2c2c74066
Lucas Berardi <lucas-l-@live.com>
2024-01-09 00:55:34 -0300

    CustomError import fixes

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express"
 import passport from 'passport'
 import { passportError, authorization } from "../utils/errorMessages.js"
 import * as sessionController from "../controllers/session.controller.js"
-import CustomError from "../services/Errors/CustomError.js"
+import CustomError from "../services/errors/CustomError.js"
 import { EErrors } from "../services/errors/enums.js"
 import { generateUserErrorInfo } from "../services/errors/info.js"
 
